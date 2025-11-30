21250213ddd52a96cd2c8e1b0cfe12daf6280679
Jessekay <kayjes00@gmail.com>
2025-11-30 11:14:30 +0200

    used Router to manage pages within our singla page app

---
+++ mynotes/src/components/ListItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ListItem = ({note}) => {
+
+  return (
+    <div>{note.body}</div>
+  )
+}
+
+export default ListItem
---
+++ mynotes/src/pages/NotePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotePage = () => {
+  return (
+    <div>
+      <h1>This is my new page</h1>
+    </div>
+  )
+}
+
+export default NotePage
