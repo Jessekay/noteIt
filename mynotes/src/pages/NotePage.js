fa33cd185748c3e24989230e3bc09678a148d09d
Jessekay <kayjes00@gmail.com>
2025-12-01 11:36:36 +0200

    added the styles and a textarea to let user be able to edit later

--- mynotes/src/pages/NotePage.js
+++ mynotes/src/pages/NotePage.js
@@ -1,15 +1,24 @@
 import notes from '../assets/data'
+import { Link } from 'react-router-dom'
+import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
 
 const NotePage = ({ match }) => {
   let noteId= match.params.id;
 
   let note = notes.find(note => note.id === Number(noteId))
 
-  if (!note) return "Note not found!";
-
   return (
-    <div>
-      <p>{note.body}</p>
+    <div className='note'>
+      <div className="note-header">
+        <h3>
+          <Link to="/">
+            <ArrowLeft />
+          </Link>
+        </h3>
+      </div>
+      <textarea value={note?.body}>
+
+      </textarea>
     </div>
   )
 }
